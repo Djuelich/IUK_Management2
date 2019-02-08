3c25c663bfbfbdfe4b5cbcfe053bc4a9bc06146d
Joker <djuelich@uos.de>
2019-02-08 22:24:55 +0100

    Initial commit

---
+++ app/src/main/java/ActivityDataSource.java
@@ -0,0 +1,9 @@
+import android.os.AsyncTask;
+
+public class ActivityDataSource extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... params){
+        return null;
+    }
+}
